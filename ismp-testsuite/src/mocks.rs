4e1d8eedf9c4eea0a72fa23f0fa27c8ab3394392
Seun Lanlege <seunlanlege@gmail.com>
2023-06-06 14:03:58 +0100

    smh clippy

--- ismp-testsuite/src/mocks.rs
+++ ismp-testsuite/src/mocks.rs
@@ -271,7 +271,7 @@ pub struct MockRouter(pub Host);
 impl IsmpRouter for MockRouter {
     fn handle_request(&self, request: Post) -> DispatchResult {
         let host = &self.0.clone();
-        let request = Request::Post(request.clone());
+        let request = Request::Post(request);
         if request.dest_chain() != host.host_state_machine() {
         } else {
             host.store_request_receipt(&request).unwrap();
